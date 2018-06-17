1cd4949a9582ec1f34d6e5c6046765f390e5d7c8
Giuseppe Silano <g.silano89@gmail.com>
2018-06-17 08:24:06 +0200

    Bug fixing in the control interface with Bebop
    A bug in the command references computing has been fixed

--- src/library/position_controller_with_bebop.cpp
+++ src/library/position_controller_with_bebop.cpp
@@ -251,8 +251,8 @@ void PositionControllerWithBebop::CalculateCommandSignals(geometry_msgs::Twist* 
     yawRate_ref_degree = control_.yawRate * (180/M_PI);
 
     double linearX, linearY, linearZ, angularZ;
-    linearX = phi_ref_degree/MAX_TILT_ANGLE;
-    linearY = theta_ref_degree/MAX_TILT_ANGLE;
+    linearX = thetha_ref_degree/MAX_TILT_ANGLE;
+    linearY = phi_ref_degree/MAX_TILT_ANGLE;
     CommandVelocity(&linearZ);
     angularZ = yawRate_ref_degree/MAX_ROT_SPEED;  
 
